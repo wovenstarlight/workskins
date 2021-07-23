1e4b8bdced4c0e26c8ffc4a09b3103f078d120c8
Devishi Ranjan <drconker@gmail.com>
2021-07-24 04:13:45 +0530

    Highlighted current skin on top toggle bar

--- skins.js
+++ skins.js
@@ -3,6 +3,12 @@ var def = $("#default"),
 	reversi = $("#reversi"),
 	lowcon = $("low-contrast");
 
+// on first load
+document.getElementById(body.attr("class")).setAttribute("class", "selected");
+
+// toggle skin
 $("#skintoggle a").click( function() {
+	$("#skintoggle a").removeClass("selected");
+	$(this).addClass("selected");
 	body.attr("class", $(this).attr("id"));
 });
