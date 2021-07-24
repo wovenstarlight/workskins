3650003cf2077cc83232d69aad7b3a80d848cbd3
Devishi Ranjan <drconker@gmail.com>
2021-07-25 02:18:05 +0530

    Updated skin bases to add a workskin toggle option

--- skins.js
+++ skins.js
@@ -1,14 +1,21 @@
 var body = $("body");
-var def = $("#default"),
-	reversi = $("#reversi"),
-	lowcon = $("low-contrast");
-
+var selected = "#" + body.attr("class");
 // on first load
-document.getElementById(body.attr("class")).setAttribute("class", "selected");
+$(selected).addClass("selected");
 
 // toggle skin
-$("#skintoggle a").click( function() {
-	$("#skintoggle a").removeClass("selected");
+$("#skintoggle a.site").click( function() {
+	$("#skintoggle a.site").removeClass("selected");
 	$(this).addClass("selected");
 	body.attr("class", $(this).attr("id"));
+});
+
+$("#skintoggle .work").click( function() {
+	if ($(this).hasClass("active")) {
+		$("link[class='custom']").prop('disabled', true);
+	}
+	else {
+		$("link[class='custom']").prop('disabled', false);
+	}
+	$(this).toggleClass("active inactive");
 });
