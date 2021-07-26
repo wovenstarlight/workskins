7ef1ce9ea4cf12874e96d44de55eb9d2f449670b
Devishi Ranjan <drconker@gmail.com>
2021-07-27 02:26:22 +0530

    Made comments more specific

--- js/skins.js
+++ js/skins.js
@@ -1,15 +1,16 @@
 var body = $("body");
 var selected = "#" + body.attr("class");
-// on first load
+// highlight selected theme on first load
 $(selected).addClass("selected");
 
-// toggle skin
+// toggle site skin
 $("#skintoggle a.site").click( function() {
 	$("#skintoggle a.site").removeClass("selected");
 	$(this).addClass("selected");
 	body.attr("class", $(this).attr("id"));
 });
 
+// toggle work skin
 $("#skintoggle .work").click( function() {
 	if ($(this).hasClass("active")) {
 		$("main > section").removeAttr("id");
