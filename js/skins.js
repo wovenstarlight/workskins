6c51a9e0a563de8c5d43a96e9a95e9c8a86f15d9
Devishi Ranjan <drconker@gmail.com>
2021-07-27 16:56:03 +0530

    Added comments

--- js/skins.js
+++ js/skins.js
@@ -3,22 +3,31 @@ var selected = "#" + body.attr("class");
 // highlight selected theme on first load
 $(selected).addClass("selected");
 
+
+
 // toggle site skin
-$("#skintoggle a.site").click( function() {
-	$("#skintoggle a.site").removeClass("selected");
-	$(this).addClass("selected");
-	body.attr("class", $(this).attr("id"));
+$("#skintoggle a.site").click( function() { // when clicking a site skin button
+	
+	$("#skintoggle a.site").removeClass("selected"); // un-highlight all
+	$(this).addClass("selected"); // highlight selected skin button
+	
+	body.attr("class", $(this).attr("id")); // set body skin
 });
+
+
 
 // toggle work skin
 $("#skintoggle .work").click( function() {
-	if ($(this).hasClass("active")) {
+
+	if ($(this).hasClass("active")) { // clicking Disable Work Skin
 		$("main > section").removeAttr("id");
 		$(this).html("Enable work skin");
 	}
-	else {
+
+	else { // clicking Enable Work Skin
 		$("main > section").attr("id", "workskin");
 		$(this).html("Disable work skin");
 	}
-	$(this).toggleClass("active inactive");
+
+	$(this).toggleClass("active"); // flip what action the button uses
 });
